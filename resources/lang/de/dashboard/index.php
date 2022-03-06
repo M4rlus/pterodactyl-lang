d9a005fc56eb10302c9bdc0135fde97fc510163a
TR <66259485+trgaming@users.noreply.github.com>
2022-03-06 21:37:56 +0100

    Update index.php

--- resources/lang/de/dashboard/index.php
+++ resources/lang/de/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Search for servers...',
-    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'search' => 'Suche nach Servern....',
+    'no_matches' => 'Es stimmen keine Server mit den Kriterien überein.',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
+    'memory_title' => 'Arbeitsspeicher',
 ];
